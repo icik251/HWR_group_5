5618f30e52eb28567b21e1be0dd118e35dc1efbc
skmuller <kennethmuller@hotmail.com>
2021-06-14 16:31:18 +0200

    push before merging with line segmentation

---
+++ main.py
@@ -0,0 +1,6 @@
+# from src.character_recognition import line_segmentation
+# from src.character_recognition import char_segmentation
+
+# from src.character_segmentation import sift
+
+# from src.style_classification import classifier
